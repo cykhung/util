d964ecc00a436286cf164dcf1c18be71e77e4062
cykhung <45437973+cykhung@users.noreply.github.com>
2022-04-22 17:47:26 -0400

    Initial commit.

---
+++ setup.m
@@ -0,0 +1,11 @@
+function setup
+
+unzip https://github.com/cykhung/svn/archive/refs/heads/master.zip c:\svn
+addpath(genpath('c:\svn'))
+svn co https://github.com/cykhung/util.git c:\util --username cykhung --password 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
+rmpath(genpath('c:\svn'))
+rmdir c:\svn s
+
+com.mathworks.mlservices.MLCommandHistoryServices.add('addpath(genpath(''c:\svn''))');  %#ok<JAPIMATHWORKS> 
+
+end
